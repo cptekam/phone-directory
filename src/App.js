ba14251913d709f8c2ef10f4dc270bb5c9dcf53a
Chandra Prakash Tekam <40147877+cptekam@users.noreply.github.com>
2018-09-25 22:36:00 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
-          Phone Directory
-      </div>
+        <div>
+            <div className="header">
+                Phone Directory
+            </div>
+            <button>Add</button>
+            <div>
+                <span>Name</span><br/>
+                <span>Phone</span>
+            </div>
+        </div>
     );
   }
 }
